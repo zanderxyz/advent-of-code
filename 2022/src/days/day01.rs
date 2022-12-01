043cdd680042cad3af17cd4105e3b5084ce3ff7a
Zander Khan <git@zander.xyz>
2022-12-01 18:48:11 +0000

    Add 2022 day 1

---
+++ 2022/src/days/day01.rs
@@ -0,0 +1,76 @@
+use std::cmp::Reverse;
+
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../../inputs/day01.txt");
+
+#[derive(Clone)]
+struct Input {
+    pub elves: Vec<Vec<usize>>,
+}
+
+impl Input {
+    pub fn new(input: &str) -> Input {
+        let mut elves = Vec::new();
+        let mut current_elf: Vec<usize> = Vec::new();
+        for line in input.lines() {
+            if line.is_empty() {
+                elves.push(current_elf);
+                current_elf = Vec::new();
+            } else {
+                let number = line.parse::<usize>().unwrap();
+                current_elf.push(number);
+            }
+        }
+        elves.push(current_elf);
+
+        Input { elves }
+    }
+}
+
+fn part1(input: &Input) -> usize {
+    input
+        .elves
+        .iter()
+        .map(|elf| elf.iter().sum())
+        .max()
+        .unwrap()
+}
+
+fn part2(input: &Input) -> usize {
+    input
+        .elves
+        .iter()
+        .map(|elf| elf.iter().sum::<usize>())
+        .sorted_by_key(|&x| Reverse(x))
+        .take(3)
+        .sum()
+}
+
+pub fn main() {
+    let input = Input::new(INPUT);
+    let answer1 = part1(&input);
+    println!("Part 1: {}", answer1);
+    let answer2 = part2(&input);
+    println!("Part 2: {}", answer2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const TEST_INPUT: &str = include_str!("../../inputs/test_day01.txt");
+
+    #[test]
+    pub fn examples() {
+        let input = Input::new(TEST_INPUT);
+        assert_eq!(part1(&input), 24000);
+        assert_eq!(part2(&input), 45000);
+    }
+
+    #[test]
+    pub fn answers() {
+        let input = Input::new(INPUT);
+        assert_eq!(part1(&input), 69528);
+        assert_eq!(part2(&input), 206152);
+    }
+}
---
+++ 2022/src/main.rs
@@ -0,0 +1,40 @@
+mod days;
+
+fn main() {
+    let day: usize = std::env::args()
+        .nth(1)
+        .expect("Please specify a day")
+        .parse()
+        .expect("Day must be an integer");
+
+    println!("Day {}", day);
+
+    match day {
+        1 => days::day01::main(),
+        // 2 => days::day02::main(),
+        // 3 => days::day03::main(),
+        // 4 => days::day04::main(),
+        // 5 => days::day05::main(),
+        // 6 => days::day06::main(),
+        // 7 => days::day07::main(),
+        // 8 => days::day08::main(),
+        // 9 => days::day09::main(),
+        // 10 => days::day10::main(),
+        // 11 => days::day11::main(),
+        // 12 => days::day12::main(),
+        // 13 => days::day13::main(),
+        // 14 => days::day14::main(),
+        // 15 => days::day15::main(),
+        // 16 => days::day16::main(),
+        // 17 => days::day17::main(),
+        // 18 => days::day18::main(),
+        // 19 => days::day19::main(),
+        // 20 => days::day20::main(),
+        // 21 => days::day21::main(),
+        // 22 => days::day22::main(),
+        // 23 => days::day23::main(),
+        // 24 => days::day24::main(),
+        // 25 => days::day25::main(),
+        _ => panic!("Solution missing for day {}", day),
+    }
+}
